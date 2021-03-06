f7bf28d5ab9b1b2fd03cd3f9825dbf73f3951915
Barbara Masinde <barbzlornah@gmail.com>
2021-03-06 14:23:12 +0300

    HeroElements

---
+++ src/components/Hero/index.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import Navbar from '../Navbar';
+import Sidebar from '../Sidebar';
+import { 
+    HeroContainer,
+    HeroContent,
+    HeroItems,
+    HeroH1,
+    HeroP,
+    HeroBtn
+ } from './HeroElements';
+
+const Hero = () => {
+    return (
+        <HeroContainer>
+            <Navbar/>
+            <Sidebar/>
+            <HeroContent>
+                <HeroItems>
+                    <HeroH1>Greatest Pizza Ever</HeroH1>
+                    <HeroP>Ready in 60 seconds</HeroP>
+                    <HeroBtn>Place Order</HeroBtn>
+                </HeroItems>
+            </HeroContent>
+        </HeroContainer>
+    )
+}
+
+export default Hero
