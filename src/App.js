4ae7dac2ffb0e5cd54ddcc31cd99f71929bd2bad
Barbara Masinde <barbzlornah@gmail.com>
2021-03-07 21:43:33 +0300

    FeatureElementsAndFooter

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import { GlobalStyle } from './globalStyles';
 import Hero from './components/Hero';
 import Products from './components/Products';
-import { productData } from './components/Products/data';
+import { productData, productDataTwo } from './components/Products/data';
+import Feature from './components/Feature';
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -11,6 +13,9 @@ function App() {
       <GlobalStyle/>
       <Hero/>
       <Products heading='Choose your favorite' data={productData} />
+      <Feature/>
+      <Products heading='Sweet Treats for You' data={productDataTwo} />
+      <Footer/>
     </Router>
   );
 }
